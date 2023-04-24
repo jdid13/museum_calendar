56fcb59b6592469925895a4fb568e45c1b1c841a
jdid13 <dwwm-nadir-benadda@mode83.onmicrosoft.com>
2023-04-24 14:08:29 +0200

    first route and api

---
+++ backend/api/restaurants.controller.js
@@ -0,0 +1,28 @@
+import { query } from "express";
+import RestaurantsDAO from "../dao/restaurantsDAO.js";
+
+export default class RestaurantsController {
+    static async apiGetRestaurants(req, res, next) {
+        const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.restaurantsPerPage, 10) : 20
+        const page = req.query.page ? parseInt(req.query.page, 10) : 0
+
+        let filters = {}
+        if (req.query.cuisine) {
+            filters.cuisine = req.query.cuisine
+        } else if (req.query.zipcode) {
+            filters.ZIPCODE = req.query.zipcode
+        } else if (req.query.name) {
+            filters.name = req.query.name
+        }
+
+        const { restaurantsList, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
+            filters,
+            page,
+            restaurantsPerPage
+        })
+
+        let response = {
+            
+        }
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
+
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize: 50,
-        wtimeout: 2500,
-        useNewUrlParse: true
+        maxpoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
     },
 )
 .catch(err => {
@@ -19,7 +21,8 @@ MongoClient.connect(
     process.exit(1)
 })
 .then(async client => {
+    await RestaurantsDAO.injectDB(client)
     app.listen(port, () => {
-        console.log('listening on port ${port}')
+        console.log(`listening on port ${port}`)
     })
 })
